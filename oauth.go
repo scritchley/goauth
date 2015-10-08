b33c9b8230e4637b41d5be7dfa46906fa2371248
Simon Critchley <si@avocet.io>
2015-10-08 19:03:14 +0100

    refactors exported methods again, settling on http.Handler interface

--- oauth.go
+++ oauth.go
@@ -10,24 +10,49 @@ const (
 	TokenEndpoint    = "/token"
 )
 
-func ApplyRoutes(mux *http.ServeMux) {
+type handler struct {
+	mux *http.ServeMux
+	ss  *SessionStore
+}
+
+func New() handler {
+	mux := http.NewServeMux()
 	mux.HandleFunc(AuthorizeEnpoint, authorizeHandler)
 	mux.HandleFunc(TokenEndpoint, tokenHandler)
+	return handler{
+		mux,
+		DefaultSessionStore,
+	}
 }
 
-func RegisterAuthorizationCodeGrant(acg AuthorizationCodeGrant, tmpl *template.Template, ss *SessionStore) {
-	tokenHandlers.AddHandler(GrantTypeAuthorizationCode, generateAuthCodeTokenRequestHandler(acg, ss))
-	authorizeHandlers.AddHandler(ResponseTypeCode, generateAuthorizationCodeGrantHandler(acg, tmpl, ss))
+// UseSessionStore overrides the referenced SessionStore implementation for the handler.
+func (h handler) UseSessionStore(ss *SessionStore) {
+	*h.ss = *ss
 }
 
-func RegisterImplicitGrant(ig ImplicitGrant, ss *SessionStore) {
-	authorizeHandlers.AddHandler(ResponseTypeToken, generateImplicitGrantHandler(ig, ss))
+// ServeHTTP implements the http.Handler interface.
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mux.ServeHTTP(w, r)
 }
 
-func RegisterResourceOwnerPasswordCredentialsGrant(ropcg ResourceOwnerPasswordCredentialsGrant, ss *SessionStore) {
-	tokenHandlers.AddHandler(GrantTypePassword, generateResourceOwnerPasswordCredentialsGrant(ropcg, ss))
+// RegisterAuthorizationGrant registers handlers for the given AuthorizationCodeGrant interface using the provided template
+// for performing user agent authentication.
+func (h handler) RegisterAuthorizationCodeGrant(acg AuthorizationCodeGrant, tmpl *template.Template) {
+	tokenHandlers.AddHandler(GrantTypeAuthorizationCode, generateAuthCodeTokenRequestHandler(acg, h.ss))
+	authorizeHandlers.AddHandler(ResponseTypeCode, generateAuthorizationCodeGrantHandler(acg, tmpl, h.ss))
 }
 
-func RegisterClientCredentialsGrant(ccg ClientCredentialsGrant, ss *SessionStore) {
-	tokenHandlers.AddHandler(GrantTypeClientCredentials, generateClientCredentialsGrantHandler(ccg, ss))
+// RegisterImplicitGrant registers handlers for the provided ImplicitGrant interface.
+func (h handler) RegisterImplicitGrant(ig ImplicitGrant) {
+	authorizeHandlers.AddHandler(ResponseTypeToken, generateImplicitGrantHandler(ig, h.ss))
+}
+
+// RegisterResourceOwnerPasswordCredentialsGrant registers handlers for the provided ResourceOwnerPasswordCredentialsGrant interface.
+func (h handler) RegisterResourceOwnerPasswordCredentialsGrant(ropcg ResourceOwnerPasswordCredentialsGrant) {
+	tokenHandlers.AddHandler(GrantTypePassword, generateResourceOwnerPasswordCredentialsGrant(ropcg, h.ss))
+}
+
+// RegisterClientCredentialsGrant registers handlers for the provided ClientCredentialsGrant interface.
+func (h handler) RegisterClientCredentialsGrant(ccg ClientCredentialsGrant) {
+	tokenHandlers.AddHandler(GrantTypeClientCredentials, generateClientCredentialsGrantHandler(ccg, h.ss))
 }
